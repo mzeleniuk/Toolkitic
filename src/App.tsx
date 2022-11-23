4acf631f769f7a6e5bee0a248561fe70698cb355
Mykhailo Zeleniuk <zell.xp@gmail.com>
2022-11-23 11:13:38 +0200

    Add the App logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import logo from './assets/logo.svg'
 import './App.css'
 
 function App() {
@@ -11,9 +11,8 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <img src={logo} className="logo" alt="Logo" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
