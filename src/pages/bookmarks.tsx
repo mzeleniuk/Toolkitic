8d6fe45d18db3dc0dc803012e31a638e48aa83b7
Mykhailo Zeleniuk <zell.xp@gmail.com>
2022-12-11 17:45:04 +0200

    Add bookmarks list

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -1,3 +1,21 @@
+import { useAppSelector } from '../hooks/useRedux'
+
 export function Bookmarks() {
-  return <h1>Bookmarks</h1>
+  const { bookmarks } = useAppSelector((state) => state.gitHub)
+
+  if (!bookmarks?.length) return <p className="text-center">No bookmarks</p>
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {bookmarks.map((bookmark) => (
+          <li key={bookmark} className='mb-2'>
+            <a href={bookmark} target="_blank">
+              {bookmark}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
